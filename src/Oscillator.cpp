359e152b3951e998840a3124113306daa108868f
Benjamin Stoneking <benjamin.stoneking@protonmail.com>
2022-09-25 22:07:56 -0600

    Add oscillator class to generate sin wave. Add utility class for often used values

---
+++ src/Oscillator.cpp
@@ -0,0 +1,17 @@
+#include "Oscillator.h"
+
+void Oscillator::init(float sample_rate) {
+    _sample_rate = sample_rate;
+    _pitch = 440.0;
+    _phase = TWO_PI / _sample_rate;
+    _phase_delta = _phase;
+}
+
+float Oscillator::get_sample() {
+    float sample = sinf(_pitch * _phase);
+    _phase += _phase_delta;
+    if(_phase > TWO_PI){
+        _phase -= TWO_PI;
+    }
+    return sample;
+}
---
+++ src/Oscillator.h
@@ -0,0 +1,12 @@
+#include <cmath>
+#include "utils.h"
+class Oscillator {
+public:
+    void init(float sample_rate);
+    float get_sample();
+private:
+    float _sample_rate = 0.0;
+    float _pitch = 0.0;
+    float _phase = 0.0;
+    float _phase_delta = 0.0;
+};
