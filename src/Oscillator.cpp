2838fcc7fb07fa9121b3cb47525ea18b5a0494a1
Benjamin Stoneking <benjamin.stoneking@protonmail.com>
2022-10-07 16:15:33 -0600

    Implement Sin, Saw and Square waveforms

--- src/Oscillator.cpp
+++ src/Oscillator.cpp
@@ -1,20 +1,65 @@
+#include "daisy_seed.h"
+#include "hardware.h"
 #include "Oscillator.h"
 
-void Oscillator::init(float sample_rate) {
+using namespace daisy;
+using namespace daisy::seed;
+
+extern DaisySeed hw;
+
+void Oscillator::init(float sample_rate)
+{
     _sample_rate = sample_rate;
     _pitch = 440.0;
     _phase = TWO_PI / _sample_rate;
     _phase_delta = _phase;
 }
 
-void Oscillator::set_pitch(float pitch_hz) {
+void Oscillator::set_pitch(float pitch_hz)
+{
     _pitch = pitch_hz;
+    _phase = 0;
+    _phase_delta = (1 / _sample_rate) * TWO_PI * _pitch;
+    //_changing_pitch = true;
+    hw.PrintLine("Pitch pending");
 }
 
-float Oscillator::get_sample() {
-    float sample = sinf(_pitch * _phase);
+void Oscillator::set_waveform(WaveForm waveform)
+{
+    _waveform = waveform;
+}
+
+float Oscillator::get_sample()
+{
+    float sample = 0.0;
+    // if(_changing_pitch) {
+    //     if(((_phase >= 0.0) && (_phase <= START_OF_CYCLE))) {
+    //         _changing_pitch = false;
+    //         _pitch = _pitch_pending;
+    //         //_phase = 0.0;
+    //         hw.PrintLine("Pitch changed");
+    //     }
+    // }
+    switch (_waveform)
+    {
+        case WaveForm::Sin: {
+            sample = sinf(_phase);
+            break;
+        }
+        case WaveForm::Square: {
+            sample = (_phase < _half_cycle) ? 1.0f: -1.0f;
+            break;
+        }
+        case WaveForm::Saw: {
+            sample = ((_phase * TWO_PI_RECIP * 2.0f) * -1.0f) * -1.0f;
+        }
+        default:
+            break;
+    }
+
     _phase += _phase_delta;
-    if(_phase > TWO_PI){
+    if (_phase > TWO_PI)
+    {
         _phase -= TWO_PI;
     }
     return sample;
--- src/Oscillator.h
+++ src/Oscillator.h
@@ -1,13 +1,31 @@
 #include <cmath>
 #include "utils.h"
+
+enum WaveForm {
+    Sin,
+    Tri,
+    Saw,
+    Square,
+    Noise
+};
+
 class Oscillator {
 public:
     void init(float sample_rate);
     void set_pitch(float pitch_hz);
+    void set_waveform(WaveForm waveform);
     float get_sample();
+
+    
+    
 private:
+    WaveForm _waveform = WaveForm::Sin;
     float _sample_rate = 0.0;
     float _pitch = 0.0;
+    float _pitch_pending = 0.0;
     float _phase = 0.0;
     float _phase_delta = 0.0;
+    const float _half_cycle = TWO_PI * 0.5;
+    bool _crossing_zero = false;
+    bool _changing_pitch = false;
 };
