591e3f16964195006749f2fa1ebc2f24f46041f3
Benjamin Stoneking <benjamin.stoneking@protonmail.com>
2022-10-23 16:05:48 -0600

    Added polyphony up to 8 notes. The process of buffering and removing notes from the voices is not optimal but may be satisfactory for now.

--- src/Synth.h
+++ src/Synth.h
@@ -6,6 +6,8 @@
 #include "Voice.h"
 #include "Oscillator.h"
 
+#define NUM_VOICES 8
+
 using namespace daisy;
 using namespace daisy::seed;
 
@@ -17,8 +19,13 @@ extern DaisySeed synth_seed;
 class Synth {
 private:
     //Synth component data members
-    Voice _voice;
-    float _amp = 0.5;
+    Voice _voices[NUM_VOICES];
+    NoteOnEvent _voice_map[NUM_VOICES];
+
+    uint8_t _voice_count = 0;
+    char _console_str[64];
+
+    float _amp = 0.1;
 
     //Audio data
     float _sample_rate = 48000.0;
@@ -34,6 +41,15 @@ public:
     void SerialDebugWriteString(const char txBuffer[],
         int bufferSize);
 
+    /*
+    These two midi note on/off commands buffer notes is a 
+    less than optimal manner. This could be a place to visit to
+    free up some processing power since there can be quite a bit
+    of time spent copying.
+    */
+    void MidiNoteOn(NoteOnEvent event);
+    void MidiNoteOff(NoteOffEvent event);
+    void PrintVoiceMap();
 };
 
 #endif
